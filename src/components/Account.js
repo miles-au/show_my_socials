b2d10111a40bd716ab1d2e0b8b678bdf0603b2e4
Miles Au <miles.d.au@gmail.com>
2019-10-12 22:03:02 -0400

    updated image path

--- src/components/Account.js
+++ src/components/Account.js
@@ -4,7 +4,7 @@ import './Account.scss';
 const Account = ({platform, linkChange}) => {
   return(
     <div className="Account bt bb b--light-gray pa2">
-      <img src={`/images/${platform}.png`} />
+      <img src={`images/${platform}.png`} />
       <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name={platform} placeholder={`Enter ${platform} url`} onChange={linkChange}/>
     </div>
   )
