3c6afa27cbcd20172b36628c6f5a1b7b2d9b35ee
Miles Au <miles.d.au@gmail.com>
2019-10-12 23:36:25 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/show_my_socials/precache-manifest.c4cabd18856661689aa6284b81b2d0be.js"
+  "/show_my_socials/precache-manifest.912676f406a9026547bacac2781f0c6f.js"
 );
 
 self.addEventListener('message', (event) => {
